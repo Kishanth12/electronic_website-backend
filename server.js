38b67e1ad55374c8aeff2ff20f93b6d27880592b
kishanth12 <kishanth1209@gmail.com>
2025-05-31 20:21:40 +0530

    cart controller

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
 import categoryRoute from './routes/categoryRoute.js';
 import brandRoute from './routes/brandRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 //app config
 const app = express();
@@ -23,6 +24,7 @@ app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/category',categoryRoute)
 app.use('/api/brand',brandRoute)
+app.use('/api/cart',cartRouter)
 app.get('/',(req,res)=>{
      res.send("hi") 
 })
