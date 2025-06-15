dba18de0e1ef44597d3e4ae12dbd75beb2cb2bbd
kishanth12 <kishanth1209@gmail.com>
2025-06-15 07:43:57 +0530

    fixed

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use('/api/category',categoryRoute)
 app.use('/api/brand',brandRoute)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
+
 app.get('/',(req,res)=>{
      res.send("hi") 
 })
